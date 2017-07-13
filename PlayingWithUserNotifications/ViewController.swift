c44fbd3c6a7fa3a3c6bb8221afafdcb833abe4c2
Sam Agnew <sagnew@eden.rutgers.edu>
2017-07-13 12:40:12 -0400

    Fix indentation

--- PlayingWithUserNotifications/ViewController.swift
+++ PlayingWithUserNotifications/ViewController.swift
@@ -13,18 +13,18 @@ import SwiftyJSON
 import UserNotifications
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func notificationButtonTapped(_ sender: Any) {
-    }
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func notificationButtonTapped(_ sender: Any) {
+  }
+  
 }
 
